ef8e78c5b0590ddf67b9892eceff07370410a1c1
Zuhairarif <162948157+Zuhairarif@users.noreply.github.com>
2026-08-04 14:04:48 +0530

    Delete models/Friends.js

--- models/Friends.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose'
-// import Users from './Users.js'
-const FriendsScheme = new mongoose.Schema({
-    name: String,
-    phone: Number,
-    friendOf: {type: mongoose.Types.ObjectId, ref:'user'}
-})
-
-export default mongoose.model('friends', FriendsScheme)
