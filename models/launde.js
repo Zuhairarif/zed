0775b13cb1a2bfc4ef784d11cdcb8ffcfaba09eb
Zuhairarif <162948157+Zuhairarif@users.noreply.github.com>
2026-08-04 14:04:35 +0530

    Delete models/launde.js

--- models/launde.js
+++
@@ -1,7 +0,0 @@
-import mongoose, { Schema } from "mongoose";
-const laundeSchema =  new mongoose.Schema({
-    name: String,
-    password: String 
-})
-export default mongoose.model('launde', laundeSchema)
-
